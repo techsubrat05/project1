0655aa1eef4f53340053c50be88585912b9ffac2
techsubrat05 <tech.subrat05@gmail.com>
2020-02-20 12:51:28 +0530

    2nd commit

--- src/main/java/com/myapp/org/TestApp/controller/FetchController.java
+++ src/main/java/com/myapp/org/TestApp/controller/FetchController.java
@@ -1,11 +1,18 @@
 package com.myapp.org.TestApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fetch")
 public class FetchController {
 
-	
+	@GetMapping("/dataFetch")
+	public String getFetchedData(@RequestParam String fetchedData) {
+
+		return fetchedData;
+	}
+
 }
